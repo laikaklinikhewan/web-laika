5621318c875623da9da2648330a1cdd36fff1456
Aditya Bimawan <aditditto@gmail.com>
2021-07-13 15:00:44 +0700

    add bg to infoklinik, bg image still buggy

--- components/CardButton.js
+++ components/CardButton.js
@@ -3,19 +3,19 @@ import PropTypes from "prop-types";
 import styles from "../styles/CardButton.module.css";
 
 const CardButton = ({ bgImgUrl, urlTo, text }) => {
-  let temp_bgImgUrl = "/assets/lobi.jpeg";
+  let tempbg = "/assets/lobi.jpeg";
   console.log(styles.cardButton);
   return (
     <a
-      href="#"
+      href={urlTo || "#"}
       className={`relative h-80 w-56 rounded-3xl overflow-hidden bg-bottom bg-cover`}
-      style={{ backgroundImage: `url(${temp_bgImgUrl})` }}
+      style={{ backgroundImage: `url(${bgImgUrl || tempbg})` }}
     >
       <div
         className={`w-full h-full justify-center bg-gradient-to-t bg-no-repeat from-black transition-all duration-500 ${styles.cardButton}`}
       ></div>
       <p className="text-2xl text-center absolute left-1/2 bottom-4 transform -translate-x-1/2 z-10 text-white">
-        {text}
+        {text || "lorem ipsum"}
       </p>
     </a>
   );
