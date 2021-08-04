4ba828701207397b05bffe77ef1f1ed6a5b6c9f4
Aditya Bimawan <aditditto@gmail.com>
2021-08-05 00:35:22 +0700

    add profile page

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "../styles/globals.css";
 import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
-import "swiper/components/scrollbar/scrollbar.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
