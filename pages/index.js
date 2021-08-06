a5b27bab037b1be540f10b5173609175f03619a8
Leo Cardhio <leocardhio@gmail.com>
2021-08-06 16:54:17 +0700

    main & pink header done

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import Navbar from '../components/navbar'
-import PinkNavbar from '../components/navbar-pink'
+import MainNavbar from '../components/MainNavbar'
+import PinkNavbar from '../components/PinkNavbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const dummyLinks = [
+    { text: "Dokter", linkUrl: "/dokter" },
+    { text: "Paramedis", linkUrl: "/paramedis" }
+  ];
   return (
     <div className={styles.container}>
-      <Navbar/>
-      <PinkNavbar/>
+      <MainNavbar/>
+      <PinkNavbar listButton={dummyLinks}/>
     </div>
   )
 }
