6808990359170092fc6ed3a95b5cbb5829f893e2
Leo Cardhio <leocardhio@gmail.com>
2021-08-07 18:12:58 +0700

    template staff complete

--- components/PinkNavbarButton.js
+++ components/PinkNavbarButton.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const PinkNavbarButton = ({ isActive, text, onClickFunc, linkTo }) => {
   return (
-    <Link href="#" passHref={true}>
+    <Link href={linkTo} passHref={true}>
       <a
         onClick={onClickFunc}
         className={`filter ${isActive ? "bg-button shadow-roundBtn" : "bg-white drop-shadow-roundBtn"} flex items-center h-11 w-36 ml-6 rounded-full justify-center`}
