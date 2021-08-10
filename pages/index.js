98d87cf64a78106d4b2b8b574b8507a78286999d
Aditya Bimawan <aditditto@gmail.com>
2021-08-11 03:14:51 +0700

    make lib/wordpress.js and connect homepage to cms with geStaticProps

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,24 @@ import InfoKlinik from "../components/InfoKlinik";
 import JamPraktek from "../components/JamPraktek";
 import HomeArticles from "../components/HomeArticles";
 import Footer from "../components/Footer";
+import { getHomepagePreviews } from "../lib/wordpress";
 
-export default function Home() {
+export default function Home({ articles, announcements }) {
   return (
     <>
       <MainNavbar />
       <IndexHero />
       <InfoKlinik />
       <JamPraktek />
-      <HomeArticles />
+      <HomeArticles articles={articles} announcements={announcements} />
       <Footer />
     </>
   );
+}
+
+export async function getStaticProps() {
+  let [articles, announcements] = await getHomepagePreviews();
+  return {
+    props: { articles, announcements },
+  };
 }
