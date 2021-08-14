a3e7554b9316c19b7f2be5adf37fca11109903f7
Leo Cardhio <leocardhio@gmail.com>
2021-08-14 15:03:54 +0700

    responsive mainNavbar

--- components/MainNavbar.js
+++ components/MainNavbar.js
@@ -35,17 +35,14 @@ const MainNavbar = () => {
 
   const [activeSect, setActiveSect] = useState(3);
   return (
-    <div className="flex fixed bg-white z-50 justify-between h-28 max-w-8xl w-screen shadow-lg text-base inline-flex">
-      <div className="relative w-logo h-logo mx-xnav my-ynav">
-        <img src="/assets/LOGO.png" alt="Laika" layout="fill" />
-      </div>
-      <div className="grid md:mr-xnav mr-xnavmin">
-        <nav className="flex space-x-10 text-tn md:text-sm text-media items-center">
+    <>
+      <div className="sm:hidden flex flex-col fixed bg-white z-50 justify-between h-28 max-w-8xl w-full shadow-lg text-base inline-flex">
+        <nav className="flex space-x-3 text-tn text-media items-center absolute right-6 mt-2">
           <a
             className="flex self-center"
             href="https://www.instagram.com/laikaklinikhewan/"
           >
-            <div className="md:p-1 p-0.5 h-igt w-igt md:h-ig md:w-ig">
+            <div className="p-0.5 h-ig w-ig">
               <img
                 src="/icons/instagram-head.svg"
                 alt=""
@@ -53,13 +50,12 @@ const MainNavbar = () => {
                 height="23px"
               />
             </div>
-            laikaklinikhewan
           </a>
           <a
             className="flex self-center"
             href="https://bit.ly/asklaikaklinikhewan"
           >
-            <div className="md:p-1 p-0.5 h-igt w-igt md:h-ig md:w-ig">
+            <div className="p-0.5 h-ig w-ig">
               <img
                 src="/icons/whatsapp-head.svg"
                 alt=""
@@ -67,39 +63,92 @@ const MainNavbar = () => {
                 height="23px"
               />
             </div>
-            bit.ly/asklaikaklinikhewan
           </a>
           <a
             className="flex self-center"
             href="https://mail.google.com/mail/u/0/?fs=1&to=laikaklinikhewan@gmail.com&tf=cm"
           >
-            <div className="md:h-mail md:w-mail h-mailt w-mailt">
+            <div className="">
               <img
                 src="/icons/mail-head.svg"
                 alt=""
-                width="20px"
-                height="20px"
+                width="24px"
+                height="24px"
               />
             </div>
-            laikaklinikhewan@gmail.com
           </a>
         </nav>
-        <nav className="flex flex-row-reverse space-x-10 space-x-reverse mt-ynav items-center md:text-base text-sm">
-          {SectorList.map((sect, index) => (
-            <MainNavbarButton
-              key={index}
-              text={sect.text}
-              sectUrl={sect.sectUrl}
-              iconUrl={sect.iconUrl}
-              isActive={activeSect === index}
-              onClickFunc={() => setActiveSect(index)}
-              height={sect.height}
-              width={sect.width}
-            />
-          ))}
-        </nav>
+        <div className="relative min-w-logo min-h-logo ml-10 mt-6">
+          <img src="/assets/LOGO.png" alt="Laika" height={65} width={65} layout="fill" />
+        </div>
+
       </div>
-    </div>
+      <div className="sm:flex hidden fixed bg-white z-50 justify-between h-28 max-w-8xl w-screen shadow-lg text-base inline-flex">
+        <div className="static min-w-logo min-h-logo lg:mx-xnav ml-10 my-ynav">
+          <img src="/assets/LOGO.png" alt="Laika" height={65} width={65} layout="fill" />
+        </div>
+        <div className="grid lg:mr-xnav mr-xnavmin">
+          <nav className="flex space-x-10 text-tn lg:text-sm text-media items-center">
+            <a
+              className="flex self-center"
+              href="https://www.instagram.com/laikaklinikhewan/"
+            >
+              <div className="lg:p-1 p-0.5 h-igt w-igt lg:h-ig lg:w-ig">
+                <img
+                  src="/icons/instagram-head.svg"
+                  alt=""
+                  width="23px"
+                  height="23px"
+                />
+              </div>
+              laikaklinikhewan
+            </a>
+            <a
+              className="flex self-center"
+              href="https://bit.ly/asklaikaklinikhewan"
+            >
+              <div className="lg:p-1 p-0.5 h-igt w-igt lg:h-ig lg:w-ig">
+                <img
+                  src="/icons/whatsapp-head.svg"
+                  alt=""
+                  width="23px"
+                  height="23px"
+                />
+              </div>
+              bit.ly/asklaikaklinikhewan
+            </a>
+            <a
+              className="flex self-center"
+              href="https://mail.google.com/mail/u/0/?fs=1&to=laikaklinikhewan@gmail.com&tf=cm"
+            >
+              <div className="lg:h-mail lg:w-mail h-mailt w-mailt">
+                <img
+                  src="/icons/mail-head.svg"
+                  alt=""
+                  width="20px"
+                  height="20px"
+                />
+              </div>
+              laikaklinikhewan@gmail.com
+            </a>
+          </nav>
+          <nav className="flex flex-row-reverse space-x-10 space-x-reverse mt-ynav items-center lg:text-base text-sm">
+            {SectorList.map((sect, index) => (
+              <MainNavbarButton
+                key={index}
+                text={sect.text}
+                sectUrl={sect.sectUrl}
+                iconUrl={sect.iconUrl}
+                isActive={activeSect === index}
+                onClickFunc={() => setActiveSect(index)}
+                height={sect.height}
+                width={sect.width}
+              />
+            ))}
+          </nav>
+        </div>
+      </div>
+    </>
   );
 };
 
